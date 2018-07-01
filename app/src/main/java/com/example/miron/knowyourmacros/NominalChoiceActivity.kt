2834300360bfed320f0c12ef7ae033ee7b10f104
m1r0n <miron.storozhev96@gmail.com>
2018-07-01 17:19:15 +0300

    #1 Get color of summary text from resources

--- app/src/main/java/com/example/miron/knowyourmacros/NominalChoiceActivity.kt
+++ app/src/main/java/com/example/miron/knowyourmacros/NominalChoiceActivity.kt
@@ -1,10 +1,14 @@
 package com.example.miron.knowyourmacros
 
 import android.app.Activity
+import android.app.PendingIntent.getActivity
 import android.content.Intent
 import android.os.Build
+import android.support.v4.content.ContextCompat
+import android.support.v4.content.res.ResourcesCompat
 import android.text.Html
 import android.text.Spanned
+import android.util.Log
 import android.view.View
 
 class NominalChoiceActivity : ChoiceActivity() {
@@ -38,11 +42,13 @@ class NominalChoiceActivity : ChoiceActivity() {
 
     @SuppressWarnings("deprecation")
     private fun createHTMLString(buttonID: Int): Spanned {
-        val buttonText: String = nominalOptions[buttonID] + "<br/><small><font color=\"#9d9b9a\"> " + nominalOptionsDescriptions[buttonID] + "</font></small>"
+        val color: String = String.format("#%06x", ContextCompat.getColor(this, R.color.summaryColor) and 0xffffff)
+        Log.i("DATADATA", color)
+        val buttonText: String = nominalOptions[buttonID] + "<br/><small><font color=\"" + color + "\"> " + nominalOptionsDescriptions[buttonID] + "</font></small>"
         return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             Html.fromHtml(buttonText, Html.FROM_HTML_MODE_LEGACY)
         } else {
-            Html.fromHtml(buttonText);
+            Html.fromHtml(buttonText)
         }
 
     }
